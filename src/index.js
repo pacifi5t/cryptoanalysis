e42f8b0415e2616f2444d23806cef4bc6894497e
Andrey Dergach <thequasar7@gmail.com>
2021-11-28 19:22:42 +0200

    Added Double Transposition cipher

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import { decrypt } from "./column-transpose.js";
+import { decrypt as decryptCT } from "./column-transpose.js";
+import { decrypt as decryptDT } from "./double-transpose.js";
 import { closeReadline, question } from "./io.js";
 
 async function main() {
   console.log("Choose cipher");
   console.log("1 - Columnar transposition");
+  console.log("2 - Double transposition");
 
   const num = parseInt(await question());
 
   switch (num) {
     case 1:
-      await decrypt();
+      await decryptCT();
       break;
-
+    case 2:
+      await decryptDT();
+      break;
     default:
       break;
   }
