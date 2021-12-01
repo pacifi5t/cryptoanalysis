488f96979d568de0154a64089953336f28c9dba7
Andrey Dergach <thequasar7@gmail.com>
2021-12-01 15:20:26 +0200

    Fixed calculations

--- src/index.js
+++ src/index.js
@@ -3,21 +3,22 @@ import { cIndex, encryptedToMatrix } from "./vigener.js";
 import { closeReadline, question } from "./io.js";
 
 async function main() {
-  // console.log("Choose cipher");
-  // console.log("1 - Columnar transposition");
-  // console.log("2 - Double transposition");
+  console.log("Choose cipher");
+  console.log("1 - Columnar transposition");
+  console.log("2 - Vigener");
 
-  // const num = parseInt(await question());
+  const num = parseInt(await question());
 
-  // switch (num) {
-  //   case 1:
-  //     await decryptCT();
-  //     break;
-  //   default:
-  //     break;
-  // }
-
-  cIndex(encryptedToMatrix(3));
+  switch (num) {
+    case 1:
+      await decryptCT();
+      break;
+    case 2:
+      cIndex(encryptedToMatrix(parseInt(await question("Enter GCD\n"))));
+      break;
+    default:
+      break;
+  }
 
   closeReadline();
 }
--- src/vigener.js
+++ src/vigener.js
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 
 const filepath = "./Шифр Виженера2.txt";
-const alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+const alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя_";
 const text = readFileSync(filepath, "utf-8").trim().toLowerCase();
 
 export function encryptedToMatrix(gcd) {
@@ -23,36 +23,29 @@ export function encryptedToMatrix(gcd) {
 
 export function cIndex(matrix) {
   const result = [];
+
   for (let i = 0; i < matrix[0].length; i++) {
     const map = new Map();
     for (let j = 0; j < matrix.length; j++) {
       const s = matrix[j][i];
-      if (map.has(s)) {
-        map.set(s, map.get(s) + 1);
-      } else {
-        map.set(s, 1);
-      }
+      map.has(s) ? map.set(s, map.get(s) + 1) : map.set(s, 1);
     }
-    console.log(map);
-
-    let f1;
-    for (let j = 0; j < alphabet.length; j++) {
-      const elem = map.get(alphabet[j]);
-      if (elem != 0 || elem != undefined) {
-        f1 = elem;
-      }
-      break;
-    }
+    // console.log(map);
 
     const m = map.has("*") ? matrix.length - 1 : matrix.length;
-    console.log(m);
+    // console.log(m);
     let sum = 0;
     for (let j = 0; j < alphabet.length; j++) {
-      sum += (map.get(alphabet[i]) ?? 0) * f1;
+      const elem = map.get(alphabet[j]) ?? 0;
+      sum += elem * (elem - 1);
     }
+
     result.push(sum / (m * (m - 1)));
     // console.log(matrix[i]);
   }
-  console.log(result);
+
+  console.log(
+    `I = ${result.reduce((total, e) => total + e, 0) / result.length}`
+  );
   return result;
 }
